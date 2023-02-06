4719a9c0cad6c10d7729e59404e9cd3fcf579289
sbleru <usareru@gmail.com>
2023-02-06 22:24:11 +0900

    chore: Add gitignore

--- apps/web/src/lib/$path.ts
+++
@@ -1,8 +0,0 @@
-export const pagesPath = {
-  "account": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/account' as const, hash: url?.hash })
-  },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
-}
-
-export type PagesPath = typeof pagesPath
