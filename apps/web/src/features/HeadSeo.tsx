7273e1f36fd6f50f37aa8f2b4f26d59e1183d827
sbleru <usareru@gmail.com>
2023-02-12 11:49:34 +0900

    chore: Add ogImageUrl prop instead of ogImagePath

--- apps/web/src/features/HeadSeo.tsx
+++ apps/web/src/features/HeadSeo.tsx
@@ -4,7 +4,7 @@ export type HeadSeoProps = {
   path: string;
   title?: string;
   description?: string;
-  ogImagePath?: string;
+  ogImageUrl?: string;
   noindex?: boolean;
   noTitleTemplate?: boolean;
 };
@@ -13,32 +13,30 @@ export type HeadSeoProps = {
  * @see https://github.com/catnose99/next-head-seo#create-wrapper-component-for-next-head-seo
  */
 export const HeadSeo: React.FC<HeadSeoProps> = (props) => {
-  const {
-    path,
-    title = "Warikan Netflix",
-    description = "Warikan Netflix",
-    ogImagePath = "/images/warikan-n.png",
-    noindex,
-    noTitleTemplate,
-  } = props;
-
   // FIXME: Set APP_ROOT_URL on enviroment variables
   const APP_ROOT_URL = "https://warikan-netflix.vercel.app";
   // e.g. APP_ROOT_URL=https://example.com
   // https://nextjs.org/docs/basic-features/environment-variables
   // const APP_ROOT_URL = process.env.NEXT_PUBLIC_APP_ROOT_URL;
 
+  const {
+    path,
+    title = "Warikan Netflix",
+    description = "Warikan Netflix",
+    ogImageUrl = APP_ROOT_URL + "/images/warikan-w.png",
+    noindex,
+    noTitleTemplate,
+  } = props;
+
   const appName = "Warikan Netflix";
 
   // Absolute page url
   const pageUrl = APP_ROOT_URL + path;
-  // Absolute og image url
-  const ogImageUrl = APP_ROOT_URL + ogImagePath;
 
   return (
     <NextHeadSeo
       title={noTitleTemplate ? title : `${title} - ${appName}`}
-      //
+      // Used to indicate to search engines which URL to evaluate when there are multiple URLs with duplicate content or very similar content on a website.
       canonical={pageUrl}
       description={description}
       robots={noindex ? "noindex, nofollow" : undefined}
